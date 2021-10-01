8688077f40b61d2d433949259326c35f605b7868
  <>
2021-10-01 22:30:01 +0300

    lataa automaattisesti käyttöön

--- kernel.sh
+++ kernel.sh
@@ -1,4 +1,5 @@
 mkdir -p /usr/src/linux/drivers/char/
 curl https://raw.githubusercontent.com/avserkkila/suomalainen-das/master/fi-das.map > /usr/src/linux/drivers/char/defkeymap.map
 cp /usr/src/linux/drivers/char/defkeymap.map /usr/share/kbd/keymaps/fi-das.map
-#echo KEYMAP=fi-das > /etc/vconsole.conf
+loadkeys fi-das
+echo KEYMAP=fi-das > /etc/vconsole.conf
