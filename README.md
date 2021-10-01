9bb335a9706019caaefd3c7f3048384f63721d42
  <>
2021-10-02 01:01:46 +0300

    päivitys

---
+++ README.md
@@ -0,0 +1,2 @@
+xorg.sh sisältää sen, mitä yleensä tarvitaan däs-näppäimistön käyttöönottamiseksi.
+kernel.sh:ta tarvitsee silloin, kun Xorg ei ole käytettävissä eli ei ole graafista käyttöliittymää, mikä toteutuu lähinnä vain Arch-Linuxin asennusvaiheessa.
--- asenna.sh
+++ asenna.sh
@@ -1,7 +1,3 @@
-curl https://raw.githubusercontent.com/avserkkila/suomalainen-das/master/evdev.xml
-curl https://raw.githubusercontent.com/avserkkila/suomalainen-das/master/fi
-curl https://raw.githubusercontent.com/avserkkila/suomalainen-das/master/keyboard
-
-mv evdev.xml /usr/share/X11/xkb/rules/evdev.xml
-mv fi /usr/share/X11/xkb/symbols/fi
-mv keyboard /etc/default/keyboard
+curl https://raw.githubusercontent.com/avserkkila/suomalainen-das/master/evdev.xml > /usr/share/X11/xkb/rules/evdev.xml
+curl https://raw.githubusercontent.com/avserkkila/suomalainen-das/master/fi > /usr/share/X11/xkb/symbols/fi
+curl https://raw.githubusercontent.com/avserkkila/suomalainen-das/master/keyboard > /etc/default/keyboard
