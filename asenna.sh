0dbc3938c9339485dd086aafc8e00e37b4575a22
  <>
2021-10-16 04:25:29 +0300

    asetelmat ovat nyt oletuksena myös käytössä, ei vain saatavilla

--- asenna.sh
+++ asenna.sh
@@ -1,3 +1,4 @@
 curl https://raw.githubusercontent.com/avserkkila/suomalainen-das/master/evdev.xml > /usr/share/X11/xkb/rules/evdev.xml
 curl https://raw.githubusercontent.com/avserkkila/suomalainen-das/master/fi > /usr/share/X11/xkb/symbols/fi
 curl https://raw.githubusercontent.com/avserkkila/suomalainen-das/master/keyboard > /etc/default/keyboard
+curl https://raw.githubusercontent.com/avserkkila/suomalainen-das/master/00-keyboard.conf > /etc/X11/xorg.conf.d/00-keyboard.conf
