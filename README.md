b0cf1c511e166f17a34ebdaa01957167e3ed7764
  <>
2022-01-14 21:30:18 +0200

    Konsolin asettelu ja asennus on nyt hyvä.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 asenna.sh sisältää sen, mitä tarvitaan das-näppäimistön käyttöönottamiseksi Xorg-ympäristössä.
-konsoli.sh on asennus linux-konsolille.
+konsoli.sh asentaa das-asettelun linux-konsolille fi-das.map-tiedostosta ja tekee siitä oletusasettelun.
--- konsoli.sh
+++ konsoli.sh
@@ -1,5 +1,20 @@
+#!/bin/sh
 mkdir -p /usr/src/linux/drivers/char/
-curl https://raw.githubusercontent.com/avserkkila/suomalainen-das/master/fi-das.map > /usr/src/linux/drivers/char/defkeymap.map
-cp /usr/src/linux/drivers/char/defkeymap.map /usr/share/kbd/keymaps/fi-das.map
+if [ -f fi-das.map ]; then
+    cp fi-das.map /usr/src/linux/drivers/char/defkeymap.map
+    cp fi-das.map /usr/share/kbd/keymaps/fi-das.map
+else
+    curl https://raw.githubusercontent.com/avserkkila/suomalainen-das/master/fi-das.map > /usr/src/linux/drivers/char/defkeymap.map
+    cp /usr/src/linux/drivers/char/defkeymap.map /usr/share/kbd/keymaps/fi-das.map
+fi
 loadkeys fi-das
-echo KEYMAP=fi-das > /etc/vconsole.conf
+
+if [ -f /etc/vconsole.conf ]; then
+    if grep -q 'KEYMAP=' /etc/vconsole.conf; then
+	sed -i 's|.*KEYMAP=[^#]*|KEYMAP=fi-das|' /etc/vconsole.conf
+    else
+	echo KEYMAP=fi-das >> /etc/vconsole.conf
+    fi
+else
+    echo KEYMAP=fi-das > /etc/vconsole.conf
+fi
