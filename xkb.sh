10e2ee796c998743b75001b06c5f9ba747a80208
  <aerkkila@users.noreply.github.com>
2022-07-18 09:49:46 +0300

    tekee tarvittaessa X11:en conf-kansion

--- xkb.sh
+++ xkb.sh
@@ -37,6 +37,7 @@ if ! env |grep -q WAYLAND_DISPLAY && env |grep -q DISPLAY; then
 fi
 
 if [ -d /etc/X11/ ]; then
+    mkdir -p /etc/X11/xorg.conf.d
     [ -f 90-oletus-xkb.conf ] && cp 90-oletus-xkb.conf /etc/X11/xorg.conf.d/ \
 	    || curl ${url}/90-oletus-xkb.conf > /etc/X11/xorg.conf.d/
 fi
