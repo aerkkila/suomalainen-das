048c2b4c34596371f4eaba4046f60bf901ab9939
  <aerkkila@users.noreply.github.com>
2022-10-22 14:32:21 +0300

    konsolin asennusta parannettu ja lisätty vaihtoehtoinen toimintavarmempi versio

--- konsoli.sh
+++ konsoli.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
-mkdir -p /usr/src/linux/drivers/char/
-if [ -f fi-das.map ]; then
-    cp fi-das.map /usr/src/linux/drivers/char/defkeymap.map
-    cp fi-das.map /usr/share/kbd/keymaps/fi-das.map
-else
-    curl https://raw.githubusercontent.com/aerkkila/suomalainen-das/master/fi-das.map > /usr/src/linux/drivers/char/defkeymap.map
-    cp /usr/src/linux/drivers/char/defkeymap.map /usr/share/kbd/keymaps/fi-das.map
-fi
-loadkeys fi-das
+kansio=/usr/src/linux/drivers/char
+
+asenna() {
+    [ -d /usr/src/linux/ ] && mkdir -p $kansio
+    if [ -f $1 ]; then
+	cp $1 /usr/share/kbd/keymaps/
+	[ -d $kansio ] && cp $1 ${kansio}/defkeymap.map
+    else
+	curl https://raw.githubusercontent.com/aerkkila/suomalainen-das/master/$1 > /usr/share/kbd/keymaps
+	[ -d $kansio ] && cp /usr/share/kbd/keymaps/$1 $kansio
+    fi
+}
+
+asenna 'fi-das.map'
+loadkeys fi-das || {
+    echo 'yritetään fi-das-1'
+    asenna 'fi-das-1.map'
+    loadkeys fi-das-1 && echo onnistui || echo ei onnistunut
+}
 
 if [ -f /etc/vconsole.conf ]; then
     if grep -q 'KEYMAP=' /etc/vconsole.conf; then
